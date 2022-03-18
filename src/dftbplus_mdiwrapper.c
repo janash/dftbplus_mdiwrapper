84c37679e8bf867eb6369f4e1d6aab6dece4a66e
Taylor Barnes <taylor.a.barnes@gmail.com>
2022-03-18 16:33:35 -0400

    Switch the wrapper to use C

--- src/dftbplus_mdiwrapper.c
+++ src/dftbplus_mdiwrapper.c
@@ -1,19 +1,16 @@
-#include <iostream>
 #include <mpi.h>
-#include <stdexcept>
 #include <string.h>
 #include "mdi.h"
 #include "dftbplus.h"
-
-using namespace std;
+#include <stdlib.h>
 
 int main(int argc, char **argv) {
 
   // Initialize the MPI environment
-  MPI_Comm world_comm;
+  MPI_Comm world_comm = MPI_COMM_WORLD;
   MPI_Init(&argc, &argv);
 
-  DftbPlus *instance;
+  DftbPlus *instance = malloc(sizeof(DftbPlus));
   dftbp_init(instance, NULL);
 
   // Synchronize all MPI ranks
