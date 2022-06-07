4d6b794adab086133e94b16b62dc771a6735a893
Taylor Barnes <taylor.a.barnes@gmail.com>
2022-06-07 16:07:38 +0000

    Add basic MDI engine structure

---
+++ src/dftbplus_engine.F90
@@ -0,0 +1,142 @@
+MODULE DFTBPLUS_ENGINE
+
+  USE mpi
+  USE ISO_C_binding
+  USE mdi,              ONLY : MDI_Init, MDI_Send, MDI_INT, MDI_CHAR, MDI_NAME_LENGTH, &
+       MDI_Accept_communicator, MDI_Recv_command, MDI_Recv, &
+       MDI_Set_execute_command_func, MDI_MPI_get_world_comm, MDI_DOUBLE, MDI_BYTE, &
+       MDI_ENGINE, MDI_Get_role, MDI_Register_command, MDI_Register_node, &
+       MDI_Register_callback, MDI_COMMAND_LENGTH, MDI_MPI_get_world_comm, &
+       MDI_Plugin_get_argc, MDI_Plugin_get_arg
+
+  IMPLICIT NONE
+
+  INTEGER, PARAMETER :: dp = selected_real_kind(15, 307)
+
+  ! MDI Communicator to the driver
+  INTEGER :: comm
+
+  ! MPI intra-communicator for this code
+  INTEGER :: world_comm
+
+  ! Flag to terminate MDI response function
+  LOGICAL :: terminate_flag = .false.
+
+CONTAINS
+
+  FUNCTION MDI_Plugin_init_dftbplus() bind ( C, name="MDI_Plugin_init_dftbplus" )
+    INTEGER :: MDI_Plugin_init_dftbplus
+    INTEGER :: ierr
+    INTEGER :: argc
+    INTEGER :: iarg
+    CHARACTER(LEN=1024) :: option
+    CHARACTER(LEN=1024) :: mdi_options
+    LOGICAL :: mdi_options_found
+
+    ! Get the command-line options from the driver
+    mdi_options_found = .false.
+    CALL MDI_Plugin_get_argc(argc, ierr)
+    DO iarg=0, argc-1
+       CALL MDI_Plugin_get_arg(iarg, option, ierr)
+       IF ( (TRIM(option) .eq. "-mdi") .or. (TRIM(option) .eq. "--mdi") ) THEN
+          IF ( argc .gt. (iarg+1) ) THEN
+             CALL MDI_Plugin_get_arg(iarg+1, mdi_options, ierr)
+             mdi_options_found = .true.
+          ELSE
+             WRITE(6,*)'ERROR: argument to -mdi option not provided'
+             MDI_Plugin_init_dftbplus = 1
+             RETURN
+          END IF
+       END IF
+    END DO
+    IF ( .not. mdi_options_found ) THEN
+       WRITE(6,*)'ERROR: -mdi option not provided'
+       MDI_Plugin_init_dftbplus = 1
+       RETURN
+    END IF
+
+    ! Call MDI_Init
+    world_comm = MPI_COMM_WORLD
+    CALL MDI_Init(mdi_options, ierr)
+
+    ! Get the MPI intra-communicator over which this plugin will run
+    CALL MDI_MPI_get_world_comm(world_comm, ierr);
+
+    ! Perform one-time operations required to establish a connection with the driver
+    CALL initialize_mdi()
+
+    ! Respond to commands from the driver
+    CALL respond_to_commands()
+
+    MDI_Plugin_init_dftbplus = 0
+  END FUNCTION MDI_Plugin_init_dftbplus
+
+
+  SUBROUTINE initialize_mdi()
+    INTEGER :: ierr, role
+
+    PROCEDURE(execute_command), POINTER :: generic_command => null()
+    TYPE(C_PTR)                         :: class_obj
+    generic_command => execute_command
+
+    ! Confirm that the code is being run as an ENGINE
+    call MDI_Get_role(role, ierr)
+    IF ( role .ne. MDI_ENGINE ) THEN
+       WRITE(6,*)'ERROR: Must run dftbplus as an ENGINE'
+    END IF
+
+    ! Register the commands
+    CALL MDI_Register_node("@DEFAULT", ierr)
+    CALL MDI_Register_command("@DEFAULT", "EXIT", ierr)
+
+    ! Connct to the driver
+    CALL MDI_Accept_communicator(comm, ierr)
+
+    ! Set the generic execute_command function
+    CALL MDI_Set_execute_command_func(generic_command, class_obj, ierr)
+
+  END SUBROUTINE initialize_mdi
+
+
+  SUBROUTINE respond_to_commands()
+    CHARACTER(len=:), ALLOCATABLE :: command
+    INTEGER :: ierr
+
+    ALLOCATE( character(MDI_COMMAND_LENGTH) :: command )
+
+    ! Respond to the driver's commands
+    response_loop: DO
+
+       ! Receive a command from the driver and broadcast it to all ranks
+       CALL MDI_Recv_command(command, comm, ierr)
+       CALL MPI_Bcast(command, MDI_COMMAND_LENGTH, MPI_CHAR, 0, world_comm, ierr)
+
+       CALL execute_command(command, comm, ierr)
+
+       IF ( terminate_flag ) EXIT
+
+    END DO response_loop
+
+    DEALLOCATE( command )
+
+  END SUBROUTINE respond_to_commands
+
+
+  SUBROUTINE execute_command(command, comm, ierr)
+    IMPLICIT NONE
+
+    CHARACTER(LEN=*), INTENT(IN)  :: command
+    INTEGER, INTENT(IN)           :: comm
+    INTEGER, INTENT(OUT)          :: ierr
+ 
+    SELECT CASE( TRIM(command) )
+    CASE( "EXIT" )
+       terminate_flag = .true.
+    CASE DEFAULT
+       WRITE(6,*)'Error: command not recognized'
+    END SELECT
+
+    ierr = 0
+  END SUBROUTINE execute_command
+
+END MODULE DFTBPLUS_ENGINE
--- src/dftbplus_mdiwrapper.F90
+++ src/dftbplus_mdiwrapper.F90
@@ -1,22 +1,47 @@
-PROGRAM DRIVER_F90
+PROGRAM DFTBPLUS_MDI
 
 USE mpi
 USE mdi
 USE dftbp_mmapi
+USE dftbplus_engine, ONLY : dp, initialize_mdi, respond_to_commands, world_comm
 
 IMPLICIT NONE
 
-   INTEGER :: world_comm, ierr
-   type(TDftbPlus) :: instance
+   INTEGER :: iarg, ierr
+   CHARACTER(len=1024) :: arg, mdi_options
 
    ! Initialize the MPI environment
-   world_comm = MPI_COMM_WORLD;
    call MPI_Init(ierr)
 
-   call TDftbPlus_init(instance, 6)
+   ! Read through all the command line options
+   iarg = 0
+   DO
+      CALL get_command_argument(iarg, arg)
+      IF (LEN_TRIM(arg) == 0) EXIT
+
+      IF (TRIM(arg) .eq. "-mdi") THEN
+         CALL get_command_argument(iarg + 1, mdi_options)
+
+         ! Initialize the MDI Library
+         world_comm = MPI_COMM_WORLD
+         CALL MDI_Init( mdi_options, ierr)
+         CALL MDI_MPI_get_world_comm( world_comm, ierr )
+
+         EXIT
+      END IF
+
+      iarg = iarg + 1
+   END DO
+
+   ! Perform one-time operations required to establish a connection with the driver
+   CALL initialize_mdi()
+
+   ! Respond to commands from the driver
+   CALL respond_to_commands()
 
    ! Synchronize all MPI ranks
-   call MPI_Barrier( world_comm, ierr )
-   call MPI_Finalize( ierr )
+   CALL MPI_Barrier( world_comm, ierr )
+   CALL MPI_Finalize( ierr )
 
-END PROGRAM DRIVER_F90
+
+END PROGRAM DFTBPLUS_MDI
