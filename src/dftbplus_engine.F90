ad111d0967d9fb8dfe06861c64288f636e353e0b
Jessica Nash <janash@vt.edu>
2022-06-09 19:55:03 +0000

    add DFTB+ initialization and <ENERGY

--- src/dftbplus_engine.F90
+++ src/dftbplus_engine.F90
@@ -8,6 +8,11 @@ MODULE DFTBPLUS_ENGINE
        MDI_ENGINE, MDI_Get_role, MDI_Register_command, MDI_Register_node, &
        MDI_Register_callback, MDI_COMMAND_LENGTH, MDI_MPI_get_world_comm, &
        MDI_Plugin_get_argc, MDI_Plugin_get_arg
+  
+  
+  USE dftbp_common_environment, ONLY : TEnvironment, TEnvironment_init
+  USE dftbp_dftbplus_inputdata, ONLY : TInputData
+  USE dftbp_dftbplus_initprogram, ONLY : TDftbPlusMain
 
   IMPLICIT NONE
 
@@ -22,9 +27,17 @@ MODULE DFTBPLUS_ENGINE
   ! Flag to terminate MDI response function
   LOGICAL :: terminate_flag = .false.
 
+  ! Create DFTB+ instance and environment
+  type(TEnvironment) :: env
+  type(TDftbPlusMain) :: main
+  
+  ! Create DFTB+ input
+  type(TInputData), allocatable :: input
+
 CONTAINS
 
   FUNCTION MDI_Plugin_init_dftbplus() bind ( C, name="MDI_Plugin_init_dftbplus" )
+
     INTEGER :: MDI_Plugin_init_dftbplus
     INTEGER :: ierr
     INTEGER :: argc
@@ -32,6 +45,8 @@ CONTAINS
     CHARACTER(LEN=1024) :: option
     CHARACTER(LEN=1024) :: mdi_options
     LOGICAL :: mdi_options_found
+
+    real*8 :: initialEnergy
 
     ! Get the command-line options from the driver
     mdi_options_found = .false.
@@ -73,6 +88,8 @@ CONTAINS
 
 
   SUBROUTINE initialize_mdi()
+    USE dftbp_dftbplus_hsdhelpers, only : parseHsdInput 
+
     INTEGER :: ierr, role
 
     PROCEDURE(execute_command), POINTER :: generic_command => null()
@@ -88,12 +105,24 @@ CONTAINS
     ! Register the commands
     CALL MDI_Register_node("@DEFAULT", ierr)
     CALL MDI_Register_command("@DEFAULT", "EXIT", ierr)
+    CALL MDI_Register_command("@DEFAULT","<ENERGY", ierr)
 
     ! Connct to the driver
     CALL MDI_Accept_communicator(comm, ierr)
 
     ! Set the generic execute_command function
     CALL MDI_Set_execute_command_func(generic_command, class_obj, ierr)
+
+    ! Initialize DFTB+
+    ! Initialize DFTB+ Environment
+    CALL TEnvironment_init(env)
+
+    ! Parse DFTB+ input
+    allocate(input)
+    CALL parseHsdInput(input)
+
+    ! Initialize variables according to input
+    call main%initProgramVariables(input, env)
 
   END SUBROUTINE initialize_mdi
 
@@ -132,6 +161,8 @@ CONTAINS
     SELECT CASE( TRIM(command) )
     CASE( "EXIT" )
        terminate_flag = .true.
+   case( "<ENERGY" )
+      call send_energy(comm)
     CASE DEFAULT
        WRITE(6,*)'Error: command not recognized'
        STOP 1
@@ -139,5 +170,26 @@ CONTAINS
 
     ierr = 0
   END SUBROUTINE execute_command
+
+
+   !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+   !
+   ! Corresponds to <ENERGY
+   ! 
+   !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  SUBROUTINE send_energy(comm)
+   USE mdi , ONLY    : MDI_DOUBLE, MDI_Send, MDI_Conversion_factor
+   USE dftbp_dftbplus_mainapi, ONLY : getEnergy, nrOfAtoms
+
+   implicit none
+   integer, intent(in)          :: comm
+   integer                      :: ierr
+   real*8                       :: etotal
+
+   ! DFTB+ uses Hartrees, so no need to convert.
+   call getEnergy(env, main, etotal)
+   call MDI_Send(etotal, 1, MDI_DOUBLE, comm, ierr)
+
+  END SUBROUTINE send_energy
 
 END MODULE DFTBPLUS_ENGINE
